7a68e4850a37f8bcc2a5e10aff9388f5d7b70957
Desenvolvedor Back-end Cleidsan <dsanmike12@gmail.com>
2023-12-02 19:48:19 -0300

    Validacao
    Inserindo dados para validação de informações, com Exceptions

--- src/main/java/com/freire/vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/freire/vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,11 @@ package com.freire.vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,10 +14,20 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços.")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido.")
     private String email;
+
+    @Length(min = 10, max = 30, message = "A senha deve conter entre (10) e (30) caracteres")
     private String password;
+
+    @Length(min = 1, max = 200, message = "Escreva uma descrição de min (1) letra a (200) letras.")
     private String description;
-    private String curriculum;
     
+    private String curriculum;
+
 }

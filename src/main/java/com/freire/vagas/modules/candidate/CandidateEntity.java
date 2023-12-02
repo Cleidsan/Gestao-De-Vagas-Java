48880ff2bf67717f49d414b01f3c8025395499b0
Desenvolvedor Back-end Cleidsan <dsanmike12@gmail.com>
2023-12-02 18:49:14 -0300

    Criando a base
    Criando a base do projeto, com o modulo de candidato, inicialmente Strings privadas.

---
+++ src/main/java/com/freire/vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,14 @@
+package com.freire.vagas.modules.candidate;
+
+import java.util.UUID;
+
+public class CandidateEntity {
+    
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+}
